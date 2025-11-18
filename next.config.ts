5a36b13adcf9adf2bdf3ea5f74dd0c5ed38ffecb
gethushan <gethushan@gmail.com>
2025-11-18 12:01:59 +0530

    Prisma Migration Setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Only ignore build errors if you're sure about your types
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
