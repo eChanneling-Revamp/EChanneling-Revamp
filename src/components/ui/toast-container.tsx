08d0f6807c708c78a9c5a5890968704cd7436b4e
gethushan <gethushan@gmail.com>
2025-12-07 07:42:00 +0530

    Created the reusable toastify component

---
+++ src/components/ui/toast-container.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ToastContainer as ReactToastifyContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export function ToastContainer() {
+  return (
+    <ReactToastifyContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+}
